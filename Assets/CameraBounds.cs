3f1e0f20a33cf5019a06db2392d02187e54c37fe
BenStark84 <Benjamin.Alexander84@gmail.com>
2019-02-10 22:27:34 -0500

    Change the Boundry Setup

---
+++ Assets/CameraBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour {
+
+    
+    Camera cam;
+    public static Vector2[] edgePoints = new Vector2[4];
+
+	// Use this for initialization
+	void Start () {
+
+        cam = Camera.main;
+        edgePoints[0] = (Vector2)cam.ScreenToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+        edgePoints[1] = (Vector2)cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
+        edgePoints[2] = (Vector2)cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, cam.nearClipPlane));
+        edgePoints[3] = (Vector2)cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, 0, cam.nearClipPlane));
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/ConfigureBoundry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigureBoundry : MonoBehaviour {
+
+    EdgeCollider2D edgeCollider;
+
+    // Use this for initialization
+    void Start () {
+        edgeCollider = gameObject.GetComponent<EdgeCollider2D>();
+        edgeCollider.points = CameraBounds.edgePoints;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
